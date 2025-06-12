31dc41c6a922803b0913c4ca4b6a1b687b53ee4d
Alexis Correa <88632261+AlexAc0226@users.noreply.github.com>
2025-06-12 16:04:51 -0300

    Autoredireccionamiento al momento de realizar un pago exitoso en mercadoPaogo

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -101,9 +101,9 @@ export class PaymentsService {
           email: userEmail,
         },
         back_urls: {
-          success: `${process.env.BACKEND_URL}/payment/success`,
-          failure: `${process.env.BACKEND_URL}/payment/failure`,
-          pending: `${process.env.BACKEND_URL}/payment/pending`,
+          success: `${process.env.BACKEND_URL}/payments/success`,
+          failure: `${process.env.BACKEND_URL}/payments/failure`,
+          pending: `${process.env.BACKEND_URL}/payments/pending`,
         },
         auto_return: 'approved',
         external_reference: orderId, // Usamos el orderId como referencia para identificar el pago
